3109edbc5f4129f28807bbd73ea6ab7f7d79c70c
Maria Alarcon <mariagaa993@gmail.com>
2020-10-14 18:08:23 -0400

    add characters filter

---
+++ src/components/reducer/Reducer.js
@@ -0,0 +1,14 @@
+export const ACTION_FILTER = "FILTER";
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+    	case ACTION_FILTER:
+          	return action.payload.data.filter(nameCharacter => 
+              	nameCharacter.name.indexOf(
+              	action.payload.query) > -1 || 
+              	nameCharacter.name.indexOf(action.payload.query) && 
+              	action.payload.query.length < 3); 
+        default:
+          	return state;
+    }
+}
