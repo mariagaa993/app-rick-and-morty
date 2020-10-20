a6fa2581102936701ed00ae7517a705ce9ce3251
Maria Alarcon <mariagaa993@gmail.com>
2020-10-20 04:21:59 -0400

    Updates

---
+++ precache-manifest.ab3b1260d8a5f2ca8e5a62f5769ba31a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15ec9c45036906a12f2bd194c38625a",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "f21843be9d7b33da04e0",
+    "url": "/rick-and-morty-app/static/css/main.c83e5817.chunk.css"
+  },
+  {
+    "revision": "242b84ca9a4b7ad9cab7",
+    "url": "/rick-and-morty-app/static/js/2.cb39dcdf.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/rick-and-morty-app/static/js/2.cb39dcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21843be9d7b33da04e0",
+    "url": "/rick-and-morty-app/static/js/main.e54fafe7.chunk.js"
+  },
+  {
+    "revision": "64bf752de5029c86e273",
+    "url": "/rick-and-morty-app/static/js/runtime-main.5288abe3.js"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/rick-and-morty-app/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "cd45d91df2151568fe047c6f9899f6da",
+    "url": "/rick-and-morty-app/static/media/logo.cd45d91d.svg"
+  }
+]);
